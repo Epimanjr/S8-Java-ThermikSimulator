3d28e253e59695d6a0f8ce3b0668ecf3a92efadc
maxime <maxime@maxime-LDLC11>
2016-02-02 11:09:21 +0100

    Allumer/Eteindre le radiateur manuellement

--- src/modele/radiateur/Radiateur.java
+++ src/modele/radiateur/Radiateur.java
@@ -19,6 +19,11 @@ public abstract class Radiateur {
     private double consigne;
 
     /**
+     * <-> Repos forcé, savoir si le radiateur est allumé ou pas.
+     */
+    private boolean eteint;
+
+    /**
      * Crée un radiateur à l'aide de plusieurs paramètres.
      *
      * @param puissanceMax
@@ -27,6 +32,7 @@ public abstract class Radiateur {
     public Radiateur(double puissanceMax, double consigne) {
         this.puissanceMax = puissanceMax;
         this.consigne = consigne;
+        this.eteint = false;
     }
 
     /**
@@ -72,6 +78,38 @@ public abstract class Radiateur {
      */
     public void setConsigne(double consigne) {
         this.consigne = consigne;
+    }
+    
+    /**
+     * Méthode qui allume le radiateur.
+     */
+    public void allumerLeRadiateur() {
+        this.setEteint(false);
+    }
+    
+    /**
+     * Méthode qui éteint le radiateur.
+     */
+    public void eteindreLeRadiateur() {
+        this.setEteint(true);
+    }
+
+    /**
+     * Pour savoir si le radiateur est allumé ou éteint.
+     *
+     * @return Vrai si allumé / Faux si éteint
+     */
+    public boolean isEteint() {
+        return eteint;
+    }
+
+    /**
+     * Permet d'allumer ou d'éteindre le radiateur.
+     *
+     * @param eteint Vrai pour l'allumer / Faux pour l'éteindre
+     */
+    public void setEteint(boolean eteint) {
+        this.eteint = eteint;
     }
 
 }
