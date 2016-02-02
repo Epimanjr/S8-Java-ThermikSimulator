58185d5ee66042084f7090015de65156a44ef09c
maxime <maxime@maxime-LDLC11>
2016-02-02 11:27:57 +0100

    Mode de fonctionnement stop

--- src/modele/radiateur/Stop.java
+++ src/modele/radiateur/Stop.java
@@ -1,14 +1,32 @@
 package modele.radiateur;
 
+import modele.Piece;
+
 /**
+ * Ce mode de fonctionnement éteint totalement le radiateur une fois 
+ * le seuil de température atteint.
+ * 
  * @author Maxime BLAISE
  * @author Antoine NOSAL
  */
 public class Stop extends Radiateur {
 
-    @Override
-    public double getThermostat(int pieceTemp, int t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Stop(double puissanceMax, double consigne) {
+        super(puissanceMax, consigne);
     }
+
+    @Override
+    public double getThermostat(Piece piece, int temperatureExterieure) {
+        return (this.isEteint()) ? 0 : calculerThermostat(piece, temperatureExterieure);
+    }
+
+    @Override
+    public void setEtatRadiateur(int temperatureAmbiante) {
+        if(temperatureAmbiante >= this.getConsigne()) {
+            this.eteindreLeRadiateur();
+        }
+    }
+
+
     
 }
