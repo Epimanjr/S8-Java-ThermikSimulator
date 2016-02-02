7ef8ff8ab2361383e7424fe73fa08fb1b57c3530
Antoine NOSAL <antoine.nosal@gmail.com>
2016-02-02 11:41:09 +0100

    finalise la classe avecTempsRepos (excepté javadoc)

--- src/modele/radiateur/AvecTempsRepos.java
+++ src/modele/radiateur/AvecTempsRepos.java
@@ -1,24 +1,47 @@
 package modele.radiateur;
 
+import modele.Piece;
+
 /**
  * @author Maxime BLAISE
  * @author Antoine NOSAL
  */
 public class AvecTempsRepos extends Radiateur {
     
+    public static int tempsRepos = 5;
     private boolean repos;
     private int reposDepuis = 0;
 
-    public AvecTempsRepos(boolean repos, double puissanceMax, double consigne) {
+    public AvecTempsRepos(double puissanceMax, double consigne) {
         super(puissanceMax, consigne);
-        this.repos = repos;
+        this.repos = false;
     }
 
     @Override
-    public double getThermostat(int pieceTemp, int t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    public double getThermostat(Piece piece, int temperatureExterieure) {
+        if (this.repos) {
+            if (this.reposDepuis >= this.tempsRepos) {
+                this.reposDepuis = 0;
+                this.repos = false;
+            }
+        }
 
+        // Calcul thermostat
+        double dT = 0;
+        if (this.repos) {
+            this.reposDepuis++;
+        } else {
+            dT = this.calculerThermostat(piece,temperatureExterieure);
+        }
+
+        return dT;
+    }
+
+    @Override
+    public void setEtatRadiateur(double temperatureAmbiante) {
+        if (temperatureAmbiante > (this.getConsigne())) {
+            this.repos = true;
+        }
+    }
     
 }
