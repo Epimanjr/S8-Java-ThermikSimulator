7bded1c6b1a7e41ebe25dfb212cbc760dcf4f85d
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-02-05 14:12:24 +0100

    Interface FXML

--- src/control/RadiateurControleur.java
+++ src/control/RadiateurControleur.java
@@ -5,11 +5,43 @@
  */
 package control;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+
 /**
  *
  * @author Maxime BLAISE
  */
-public class RadiateurControleur {
-    
-    
+public class RadiateurControleur implements Initializable {
+
+    @FXML
+    private LineChart<Double, Double> graph;
+    @FXML
+    private Button but;
+
+    private final LineChart.Series<Double, Double> serieInterieure = new LineChart.Series<>();
+    private final LineChart.Series<Double, Double> serieExterieure = new LineChart.Series<>();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Nommage des séries
+        serieInterieure.setName("Température intérieure");
+        serieExterieure.setName("Température extérieure");
+        // Ajout des séries au graphe
+        graph.getData().add(serieInterieure);
+        graph.getData().add(serieExterieure);
+    }
+
+    @FXML
+    private void handleButton(ActionEvent event) {
+        serieInterieure.getData().add(new XYChart.Data<>(
+                10.0, 10.0
+        ));
+    }
 }
