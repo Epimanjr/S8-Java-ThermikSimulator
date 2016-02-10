5e100e96b2de0aa5d8c57fbb6d03786c94053ca4
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-02-10 14:55:56 +0100

    Modif controlleur

---
+++ src/control/ParametresControl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author Maxime
+ */
+public class ParametresControl {
+    
+    // Paramètre isolation
+    public static double minIsolation = 0.01;
+    public static double maxIsolation = 1;
+}
